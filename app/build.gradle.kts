8ec46a46bfeab14509eaaee9e3063a006d2854d2
Nathan Garretson <nathan@isidorefarm.com>
2023-02-21 14:06:00 -0500

    - dockerize app - add jar building to gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("groline.uploader.kotlin-application-conventions")
+    java
 }
 
 dependencies {
@@ -9,4 +10,21 @@ dependencies {
 application {
     // Define the main class for the application.
     mainClass.set("groline.uploader.app.Uploader")
+}
+
+tasks.withType<Jar> {
+    // Otherwise you'll get a "No main manifest attribute" error
+    manifest {
+        attributes["Main-Class"] = "groline.uploader.app.Uploader"
+    }
+    // To avoid the duplicate handling strategy error
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    // To add all of the dependencies
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
 }
